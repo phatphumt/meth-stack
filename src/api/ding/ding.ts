c756ad684d309b5ac52269d28294f3e86accbad2
Crunchitect <95293374+Crunchitect@users.noreply.github.com>
2024-02-01 23:13:27 +0700

    File-Based Routing At Home.

--- src/api/ding/ding.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-import dingGET from "./get";
-
-const ding = new Elysia({prefix: "/ding"}).use(dingGET)
-
-export default ding
--- src/api/todos/toggle/toggle.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-import todoTogglePOST from "./post";
-
-const todosToggle = new Elysia({ prefix: "/todos/toggle" }).use(todoTogglePOST);
-
-export default todosToggle;
